6947a1faef377e5f812edfae4b4a452feb1ed173
Steven <stevenpjohnson@pm.me>
2023-06-05 22:38:36 -0500

    Updates

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000],
     }],
   },
   options: {
