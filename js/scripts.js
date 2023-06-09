eab669db60869b6dcfd2202c8ed7185cda45dcaf
Steven <stevenpjohnson@pm.me>
2023-06-09 17:49:29 -0500

    dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,29 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
+    // Dark mode logic
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "dark") {
+      document.body.classList.add("dark-theme");
+      btn.innerHTML = '<i class="fas fa-sun"></i> Toggle Theme';
+    } else if (currentTheme === "light") {
+      document.body.classList.add("light-theme");
+      btn.innerHTML = '<i class="fas fa-moon"></i> Toggle Theme';
+    }
+
+    btn.addEventListener("click", function () {
+      document.body.classList.toggle("dark-theme");
+      document.body.classList.toggle("light-theme");
+
+      if (document.body.classList.contains("light-theme")) {
+        btn.innerHTML = '<i class="fas fa-moon"></i> Toggle Theme';
+        localStorage.setItem("theme", "light");
+      } else {
+        btn.innerHTML = '<i class="fas fa-sun"></i> Toggle Theme';
+        localStorage.setItem("theme", "dark");
+      }
+    });
+
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -44,4 +67,16 @@ window.addEventListener('DOMContentLoaded', event => {
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
+
+    const btn = document.querySelector(".btn-toggle");
+
+    // Get all the image containers
+    const imageContainers = document.querySelectorAll('.image-container');
+
+    // Attach a click event listener to each image container
+    imageContainers.forEach(container => {
+      container.addEventListener('click', () => {
+        container.classList.toggle('hide');
+      });
+    });
 });
