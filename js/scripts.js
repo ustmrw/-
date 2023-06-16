667393d3e87e7fe2fb45309cbbc5076ee77ccf06
Steven <stevenpjohnson@pm.me>
2023-06-15 22:59:16 -0500

    effects

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,30 @@
 // 
 window.addEventListener('DOMContentLoaded', event => {
 
+  //track window
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function handleScroll() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((element) => {
+      if (isElementInViewport(element)) {
+        element.classList.add('visible');
+      }
+    });
+  }
+  
+  // Attach the scroll event listener
+  window.addEventListener('scroll', handleScroll);
+  
+
   // Toggle the side navigation
   const sidebarToggle = document.body.querySelector('#sidebarToggle');
   if (sidebarToggle) {
