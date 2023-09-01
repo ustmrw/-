d12119e04eecb7dc59c25be4aff0043bbf5bd89f
Steven <stevenpjohnson@pm.me>
2023-09-01 13:40:58 -0500

    search

--- js/scripts.js
+++ js/scripts.js
@@ -143,6 +143,21 @@ window.addEventListener('DOMContentLoaded', event => {
   });
 });
 
+function search_items() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('nav-link');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
+
 (function ($) {
   "use strict";
 
